2953adf7d3f2159f92718c2ce255298ef074a723
codeniaaz <muktarabdiaziz12@gmail.com>
2023-11-28 11:01:33 -0800

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in range(ord('z'), ord('a') - 1, -1):
-    print(f"{chr(i)}" if i % 2 == 0 else f"{chr(i).upper()}", end="")
+    print("{}".format(chr(i)) if i % 2 == 0 else "{}".format(chr(i).upper()), end="")
