81a1a5f31daa3a5351339cfd9b79147b3db28261
codeniaaz <muktarabdiaziz12@gmail.com>
2023-11-28 07:36:04 -0800

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for digit1 in range(9):
     for digit2 in range(digit1 + 1, 10):
         print("{}{}".format(digit1 % 10, digit2 % 10), end=", "
-                if not (digit1 == 8 and digit2 == 9) else "")
+if not (digit1 == 8 and digit2 == 9) else "")
 
 print()
