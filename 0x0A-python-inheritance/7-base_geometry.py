f6e1d92d76f6b50f9275872a5c59a804618e15ec
Codeniaaz <muktarabdiaziz12@gmail.com>
2024-01-08 17:29:17 +0300

    validates value

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-"""An class that has a public method"""
+"""
+This module contains one class: BaseGeometry
+"""
 
 
 class BaseGeometry:
-    ''' it has a public method'''
+    """ contains two public instance methods """
     def area(self):
-        ''' that raises an Exception with a message'''
+        """
+        raises an Exception with the message area() is not implemented
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        '''def integer_validator(self, name, value)'''
-
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+        """ validates value """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
