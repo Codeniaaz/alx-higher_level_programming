d453e3685112d45d99b04329db209d0380578b67
codeniaaz <muktarabdiaziz12@gmail.com>
2024-03-29 05:35:32 +0300

    autopep

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-#script that takes a URL and an email as command-line arguments
+# script that takes a URL and an email as command-line arguments
 
 import urllib.parse
 import urllib.request
 import sys
+
 
 def send_post(url, email):
     data = urllib.parse.urlencode({'email': email}).encode('utf-8')
@@ -14,9 +15,9 @@ def send_post(url, email):
         print("Response body:")
         print(body)
 
+
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
 
     send_post(url, email)
-
