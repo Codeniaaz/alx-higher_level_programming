d0389b701717a8e44385168765a342b98f2a5d9c
codeniaaz <muktarabdiaziz12@gmail.com>
2024-04-23 20:41:47 +0300

    0-readme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const Filepath = process.argv[2];
+const content = process.argv[3];
+fs.writeFile(Filepath, content, 'utf-8', (err( => {
+	if (err) throw err;
+});
