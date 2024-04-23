a1c74211a9058c1f123e26dcec2382c3168bd07f
codeniaaz <muktarabdiaziz12@gmail.com>
2024-04-23 20:49:08 +0300

    0-readme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -6,7 +6,5 @@ const content = process.argv[3];
 fs.writeFile(Filepath, content, 'utf-8', (err) => {
   if (err) {
     console.error(err);
-  } else {
-    console.log('Done successfully', content);
   }
 });
